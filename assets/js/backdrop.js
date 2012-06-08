00b3ef5c15681d75493149632c0b951f86ceebca
David Miller <davidfmiller@me.com>
2012-06-07 23:21:14 -0700

    Updated backdrop

--- assets/js/backdrop.js
+++ assets/js/backdrop.js
@@ -6,46 +6,63 @@ YUI.add('backdrop', function(Y) {
 
     'use strict';
 
-    var Backdrop = function() {
+    /* 
+     *
+     * @param config (object)
+     *  'url' (string) - the path to the background image
+     *  'id' (string, optional) - 
+     *  'duration' (float, optional) - seconds
+     */
+    var Backdrop = function(config) {
       Backdrop.superclass.constructor.apply(this, arguments);
+
+      this.set('url', config.url);
+      this.set('id', config.hasOwnProperty('id') ? config.id : null);
+      this.set('duration', config.hasOwnProperty('duration') ? config.duration : null);
+
+      if (! this.get('url')) { return null; }
+
+      var img = new Image(), o = {};
+
+      o.$ = this;
+      o.node = Y.Node.create('<div id="' + this.get('id') + '"></div>');
+
+      img.onload = function() {
+        Y.one('body').append(o.node);
+        o.node.setStyle('backgroundImage', 'url(' + this.src + ')');
+        o.$.resize();
+        o.node.transition({
+          'opacity' : 1,
+          'duration' : o.$.get('duration')
+        }, function() { o.$.fire('drop'); });
+
+        Y.on('windowresize', function() { o.$.resize(); });
+      };
+      img.src = this.get('url');
+
+      return this;
     };
+
+    Backdrop.ATTRS = {
+      url : {
+        value : null,
+        writeOnce : true
+      },
+      id : {
+        value : null,
+        setter : function(id) { return id ? id : 'backdrop'; },
+        writeOnce : true,
+      },
+      duration : {
+        value : null,
+        writeOnce : true,
+        setter : function(i) { return (i ? parseFloat(i, 10) : 1); }
+      }
+    };
+
 
     Y.Backdrop = Y.extend(Backdrop, Y.Base,
     {
-
-      /* 
-       *
-       * @param config (object)
-       *  'url' (string) - the path to the background image
-       *  'id' (string, optional) - 
-       *  'duration' (float, optional) - seconds
-       */
-      initializer : function(config) {
-
-        this.set('url', config.url);
-        this.set('id', config.hasOwnProperty('id') ? config.id : 'backdrop');
-        this.set('duration', config.hasOwnProperty('duration') ? parseFloat(config.duration, 10) : 1);
-
-        var img = new Image(), o = {};
-
-        o.$ = this;
-        o.node = Y.Node.create('<div id="' + this.get('id') + '"></div>');
-
-        img.onload = function() {
-          Y.one('body').append(o.node);
-          o.node.setStyle('backgroundImage', 'url(' + this.src + ')');
-          o.$.resize();
-          o.node.transition({
-            'opacity' : 1,
-            'duration' : o.$.get('duration')
-          }, function() { o.$.fire('drop'); });
-
-          Y.on('windowresize', function() { o.$.resize(); });
-        };
-        img.src = this.get('url');
-
-        return this;
-      },
 
       /*
        *
@@ -62,6 +79,15 @@ YUI.add('backdrop', function(Y) {
         return this;
       },
 
+      /*
+       *
+       * @return string
+       */
+      toString : function() {
+        return '[Backdrop]';
+      }
+
+
       /* 
        *
        *
@@ -75,29 +101,8 @@ YUI.add('backdrop', function(Y) {
       },
        */
 
-      /*
-       *
-       * @return string
-       */
-      toString : function() {
-        return '[Backdrop]';
-      },
-
-      'NAME' : 'backdrop',
-      'ATTRS' : {
-        url : {
-          value : null,
-          writeOnce : true
-        },
-        id : {
-          value : null,
-          writeOnce : true,
-        },
-        duration : {
-          value : null,
-          writeOnce : true
-        }
-      }
+//      'NAME' : 'backdrop',
+      
     });
 
   }, '3.3.1', { requires : ['node', 'base', 'event', 'event-resize', 'transition' ] });
