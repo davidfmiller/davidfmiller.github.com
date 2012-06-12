00304c7321887a170cff2bbacbd37843867f5248
David Miller <davidfmiller@me.com>
2012-06-11 23:30:22 -0700

    Update backdrop

--- assets/js/backdrop.js
+++ assets/js/backdrop.js
@@ -22,11 +22,13 @@ YUI.add('backdrop', function(Y) {
       this.set('url', config.hasOwnProperty('url') ? config.url : null);
       this.set('id', config.hasOwnProperty('id') ? config.id : null);
       this.set('duration', config.hasOwnProperty('duration') ? config.duration : null);
+      this.set('styles', config.hasOwnProperty('styles') ? config.styles : null);
 
       if (this.get('url')) { this.drop(); }
 
       return this;
-    };
+    },
+    DEFAULT_STYLES = { 'position' : 'top left', 'repeat' : 'repeat-none', 'attachment' : 'fixed', 'size' : 'auto' };
 
     Backdrop.ATTRS = {
       url : {
@@ -36,6 +38,9 @@ YUI.add('backdrop', function(Y) {
         value : null,
         setter : function(id) { return id || 'backdrop'; },
         writeOnce : true
+      },
+      styles : {
+        value : null
       },
       duration : {
         value : 1,
@@ -53,6 +58,15 @@ YUI.add('backdrop', function(Y) {
         this.set('id', null);
         this.set('duration', null);
         this.set('url', null);
+        this.set('styles', null);
+      },
+
+      _applyStyles : function(node, styles) {
+        for (var s in styles) {
+          if (styles.hasOwnProperty(s)) {
+            node.setStyle('background' + s.charAt(0).toUpperCase() + s.substr(1), styles[s]);
+          }
+        }
       },
 
       /**
@@ -64,9 +78,11 @@ YUI.add('backdrop', function(Y) {
 
         if (typeof config === 'string') {
           this.set('url', config);
+          this.set('styles', null)
         } else {
           if (config.hasOwnProperty('url')) { this.set('url', config.url); }
           if (config.hasOwnProperty('duration')) { this.set('duration', config.duration); }
+          this.set('styles', config.hasOwnProperty('styles') ? config.styles : null);
         }
 
         var img = new Image(), o = {};
@@ -76,12 +92,29 @@ YUI.add('backdrop', function(Y) {
 
         img.onload = function() {
           Y.one('body').append(o.node);
-          o.node.setStyle('backgroundImage', 'url(' + this.src + ')');
+
+          var s = Y.merge(DEFAULT_STYLES, o.$.get('styles'));
+
+          s.image = 'url(' + this.src + ')';
+          o.$._applyStyles(o.node, s);
+
+          //o.node.setStyle('backgroundImage', 'url(' + this.src + ')');
           o.$.resize();
           o.node.transition({
             'opacity' : 1,
             'duration' : o.$.get('duration')
-          }, function() { o.$.fire('drop', o.$.get('url') ); Y.one('body').setStyle('backgroundImage', 'url(' + img.src + ')'); o.node.remove();  });
+          }, function() {
+            o.$.fire('drop', o.$.get('url') );
+
+            var styles = Y.merge(DEFAULT_STYLES, o.$.get('styles')),
+                body = Y.one('body');
+
+            styles.image = 'url(' + img.src + ')';
+
+            o.$._applyStyles(body, styles);
+
+            o.node.remove();
+          });
 
           Y.on('windowresize', function() { o.$.resize(); });
         };
