9e7b550e6588d961b5548b101a53d69c196a4e4c
dmiller <dmiller@edmunds.com>
2016-01-07 16:36:46 -0800

    update popover, popovers for links

--- scripts/rmr.js
+++ scripts/rmr.js
@@ -5,8 +5,6 @@
 /* screen */
 !function(){var e,n,t,r=["webkit","moz","o","ms","khtml"],o="rmr-screen";if(e={prefix:"",supported:!1,isFullScreen:function(){return!1},exit:function(){},request:function(){},eventName:null},"undefined"!=typeof document.cancelFullScreen)e.supported=!0;else for(n=0;n<r.length;n++)if(t=r[n],"undefined"!=typeof document[t+"CancelFullScreen"]){e.supported=!0,e.prefix=t;break}e.supported&&(e.eventName=e.prefix+"fullscreenchange",e.request=function(e){return t?e[t+"RequestFullScreen"]():e.requestFullScreen()},e.exit=function(e){return t?document[t+"CancelFullScreen"]():document.cancelFullScreen()},e.isFullScreen=function(){var e=null;switch(t){case"webkit":e=document.webkitIsFullScreen;break;case"moz":e=document.mozFullScreenElement;break;default:document.hasOwnProperty("fullScreen")?e=document.fullScreen:document.hasOwnProperty("fullscreen")&&(e=document.fullscreen),e=document[t+"FullScreen"]}return e}),window.Screen=function(n){this.node=document.querySelector(n),this.events={exit:function(){},fullscreen:function(){}};var t=this;this.listener=function(e){t.isFullScreen()?(t.events.fullscreen(),t.node.classList.add(o)):(t.events.exit(),t.node.classList.remove(o))},"moz"==e.prefix?document.addEventListener("mozfullscreenchange",this.listener):t.node.addEventListener(e.eventName,this.listener)},window.Screen.prototype.isSupported=function(){return e.supported},window.Screen.prototype.request=function(){e.request(this.node)},window.Screen.prototype.isFullScreen=function(){return e.isFullScreen()},window.Screen.prototype.on=function(e,n){this.events[e]=n},window.Screen.prototype.toggle=function(){return this.isFullScreen()?this.exit():this.request()},window.Screen.prototype.exit=function(){e.exit()},window.Screen.prototype.toString=function(){return"Screen <"+this.node+">"}}();
 
-
-
 // backdrop
 (function() {
 
@@ -88,7 +86,6 @@
 
     this.id = config.hasOwnProperty('id') ? config.id : 'backdrop';
     this.url = config.hasOwnProperty('url') ? config.url : null;
-    this.duration = config.hasOwnProperty('duration') ? config.duration :1;
     this.styles = config.hasOwnProperty('styles') ? config.styles : null;
 
     if (this.url) {
@@ -117,7 +114,6 @@
       this.styles = null;
     } else if (config) {
       if (config.hasOwnProperty('url')) { this.url = config.url; }
-      if (config.hasOwnProperty('duration')) { this.duration = config.duration; }
       if (config.hasOwnProperty('styles')) { this.styles = config.styles; }
     }
 
@@ -145,7 +141,7 @@
       var val = 0;
       var anim = function() {
 
-        val += 0.02;
+        val += 0.04;
         o.node.style.opacity = val;
 
         if (val >= 1) {
@@ -163,7 +159,7 @@
         }
       };
 
-      var interval = window.setInterval(anim, o.$.duration / 100);
+      var interval = window.setInterval(anim, 10);
 
       if (! RESIZE_LISTENED) {
         window.addEventListener('resize', function(e) {
@@ -189,7 +185,7 @@
     document.body.style.minHeight = window.innerHeight + 'px';
 
     if (node) {
-        setStyles(node, { width : rect.width + 'px',  height : rect.height + 'px' });
+      setStyles(node, { width : rect.width + 'px',  height : rect.height + 'px' });
     }
 
     return this;
@@ -201,7 +197,6 @@
 
 }());
 
-
 // popover
 (function() {
 
@@ -210,7 +205,7 @@
   var
 
   //
-  VERSION = '0.1.2',
+  VERSION = '0.1.3',
 
   // node attribute
   ATTR = 'data-popover',
@@ -355,6 +350,7 @@
       n,
       arrow,
       targetRect = getRect(target),
+//      bodyRect = getRect(document.body),
       popoverRect,
       popoverXY,
       arrowXY,
@@ -379,6 +375,9 @@
           data = { content : e.target.getAttribute(ATTR) };
         }
       }
+
+      // if there's no content and no specific class, abort since it's an empty popover
+      if (! data.content && ! data['class']) { return; }
 
       data['class'] = (data['class'] ? data['class'] : '') + ' rmr-popover';
       data.id = target.getAttribute('id') + '-popover';
@@ -408,6 +407,14 @@
 
       arrowXY = [popoverXY[0], popoverXY[1]];
       arrowXY[0] = popoverRect.width / 2 - 6;
+
+      if (popoverXY[0] < 0) {
+        popoverXY[0] = 5;
+        arrowXY[0] = targetRect.left + targetRect.width / 2 - 10;
+      } else if (popoverXY[0] + popoverRect.width > window.innerWidth ) {
+        popoverXY[0] = window.innerWidth - popoverRect.width - 5;
+        arrowXY[0] = popoverRect.width - targetRect.width / 2;
+      }
 
       target.setAttribute('aria-describedby', data.id);
       n.setAttribute('style', 'left: ' + parseInt(popoverXY[0], 10) + 'px; top: ' + parseInt(popoverXY[1], 10) + 'px');
@@ -456,10 +463,11 @@
         target.removeAttribute('aria-describedBy');
         try {
           var pop = pops[id + '-popover'];
-          pop.parentNode.removeChild(pop);
+
           delete pops[id + '-popover'];
 
-          if (pop.classList.contains('pop')) {
+          if (pop) {
+            pop.parentNode.removeChild(pop);
             $.events.unpop(target, pop);
           }
 
@@ -526,14 +534,13 @@
 
   'use strict';
 
+  // mobile browser check
   if (
     (function() {
     var check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
     return check;}())
   ) { return; }
-
-//  if (navigator.appVersion.indexOf("Mobile") == -1) { return; }
 
   var
   /*
@@ -553,9 +560,7 @@
     return ret;
   };
 
-  var dropper = new Backdrop({
-    'id' : 'backdrop'
-  }),
+  var dropper = new Backdrop(),
   styles = {
     'backdrop' : { 'size' : 'cover' },
     'chinatown' : { 'repeat' : 'repeat-y', 'size' : 'cover' },
