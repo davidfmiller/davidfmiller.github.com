0e47d4647e245db4d84f4df9263929c22de10be5
David Miller <davidfmiller@me.com>
2023-10-19 08:44:39 -0700

    forematter

--- forematter/test/json.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo <<<JSON
-{ "home" : "https://readmeansrun.com", "forematter" : "https://readmeansrun.com/forematter", "email" : "name@domain.com", "bool" : true , "object" : { "string" : "Hello world", "number" : 42 }, "empty" : [ ], "nested" : [ "abc", true, { "foo" : "bar" } ] }
-JSON;
-
-?>
